7aacc53453ce0c1dded0272da5d1c6d008aa0536
theyujisamfull <theyujisamfulll@gmail.com>
2019-02-22 02:15:45 -0300

    readme

--- Tabela_datas_proximas.php
+++ Tabela_datas_proximas.php
@@ -2,7 +2,7 @@
 	<thead><tr><th>Prof</th><th>Timer</th><th>Evento</th><th>Data</th><th>Sem</th></tr></thead>
 	<?php
 	// Puxa os dados do banco de dados e printa a tabela
-	$resp = mysqli_query($link,"SELECT * FROM datasproximas ORDER BY timer DESC"); 
+	$resp = mysqli_query($link,"SELECT * FROM datasproximas ORDER BY id DESC"); 
 	while($dado=$resp->fetch_array()){ echo "
 		<tr><td>".$dado['prof']."</td><td>".$dado['timer']."</td><td>".$dado['evento']."</td><td>".$dado['data']."</td><td>".$dado['semana']."</td></tr>
 	";} 
